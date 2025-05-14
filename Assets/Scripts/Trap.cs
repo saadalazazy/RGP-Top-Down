e9aa83181bae3c7f4e3ed68e23a20c7eb4d8eb88
saad <90611647+saadalazazy@users.noreply.github.com>
2025-05-14 05:10:32 +0300

    add trap

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField] float closeTrapTime;
+    [SerializeField] float openTrapTime;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(OpenTrap());
+    }
+    IEnumerator OpenTrap()
+    {
+        yield return new WaitForSeconds(openTrapTime);
+        animator.CrossFadeInFixedTime("Trap", 0.1f);
+        StartCoroutine(CloseTrap());
+    }
+    IEnumerator CloseTrap()
+    {
+        yield return new WaitForSeconds(openTrapTime);
+        animator.CrossFadeInFixedTime("Close Trap", 0.1f);
+        StartCoroutine(OpenTrap());
+    }
+
+
+
+
+}
